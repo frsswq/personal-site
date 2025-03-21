f08040811b4097ac0d8b0f3a6bf98f4403526088
frsswq <farissaifuddin.work@gmail.com>
2025-03-21 22:42:28 +0700

    use grid

--- src/components/explore/ExploreMain.svelte
+++ src/components/explore/ExploreMain.svelte
@@ -8,60 +8,28 @@
 
 <section>
 	<h1
-		class="title pt-12 text-center text-[2rem] leading-none font-bold tracking-tighter text-zinc-700
+		class="title pt-8 text-center text-[2rem] leading-none font-bold tracking-tighter text-zinc-700 md:pt-12
 			md:text-[3.5rem]"
 	>
 		Handcrafted Designs<br />Built from Scratch
 	</h1>
 	<p
-		class="subtitle pt-4 text-center text-[1rem] font-medium tracking-tight text-zinc-500 md:text-2xl"
+		class="subtitle pt-2 text-center text-[1rem] font-medium tracking-tight text-zinc-500 md:pt-4 md:text-2xl"
 	>
-		Zero dependencies. Good design. Raw code.
+		Zero dependencies. Pure vibes.
 	</p>
-	<div class="flex w-full shrink-0 flex-col justify-center pt-12 md:flex-row md:gap-x-4">
+	<div class="flex w-full flex-col justify-center pt-8 md:flex-row md:gap-x-4 md:pt-12">
 		<div
-			class="flex h-fit w-fit flex-col items-center justify-center gap-2.5 bg-white md:items-end md:gap-4"
+			class="grid h-fit w-full grid-rows-1 items-center justify-center gap-2.5 bg-white md:w-fit md:grid-cols-2
+				md:items-start md:gap-4 xl:grid-cols-3"
 		>
 			{#each designItems as Item, index}
-				{#if index % 3 === 0}
-					<div
-						class="flex h-fit w-[300px] items-center justify-center rounded-xl border border-zinc-200 bg-zinc-50 p-6.25
-							md:w-[375px]"
-					>
-						<Item />
-					</div>
-				{/if}
-			{/each}
-		</div>
-		<div
-			class="mt-2.5 flex h-fit w-fit flex-col items-center justify-center gap-2.5 bg-white md:mt-0
-				md:items-center md:gap-4"
-		>
-			{#each designItems as Item, index}
-				{#if index % 3 === 1}
-					<div
-						class="flex h-fit w-[300px] items-center justify-center rounded-xl border border-zinc-200 bg-zinc-50 p-6.25
-							md:w-[375px]"
-					>
-						<Item />
-					</div>
-				{/if}
-			{/each}
-		</div>
-
-		<div
-			class="mt-2.5 flex h-fit w-fit flex-col items-center justify-center gap-2.5 bg-white md:mt-0 md:items-start
-				md:gap-4"
-		>
-			{#each designItems as Item, index}
-				{#if index % 3 === 2}
-					<div
-						class="flex h-fit w-[300px] items-center justify-center rounded-xl border border-zinc-200 bg-zinc-50 p-6.25
-							md:w-[375px]"
-					>
-						<Item />
-					</div>
-				{/if}
+				<div
+					class="flex h-fit w-[300px] items-center justify-center rounded-xl border border-zinc-200 bg-zinc-50 p-6.25
+						md:w-[375px]"
+				>
+					<Item />
+				</div>
 			{/each}
 		</div>
 	</div>

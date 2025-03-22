49fdb0005c6cef7cbbc46e87752c6d23793643df
frsswq <farissaifuddin.work@gmail.com>
2025-03-22 20:37:41 +0700

    add navigation

---
+++ src/components/icons/IconamoonSignDivisionSlashLight.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let { className = '' } = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1em"
+	height="1em"
+	viewBox="0 0 24 24"
+	class={className}
+	><!-- Icon from All by undefined - undefined --><path
+		fill="none"
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="1.5"
+		d="M16 4L8 20"
+	/></svg
+>

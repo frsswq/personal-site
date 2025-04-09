89d79f2909b085f5277f82436c68c4592e49807d
frsswq <farissaifuddin.work@gmail.com>
2025-04-09 13:21:35 +0700

    improve perf on /design

--- src/components/design/DesignMain.svelte
+++ src/components/design/DesignMain.svelte
@@ -1,26 +1,11 @@
 <script lang="ts">
-	import { innerWidth } from 'svelte/reactivity/window';
-	import { tick } from 'svelte';
 	import type { Component } from 'svelte';
-
-	import SvgSpinnersBarsRotateFade from '../icons/SvgSpinnersBarsRotateFade.svelte';
 
 	const paths: Record<string, { default: Component }> = import.meta.glob('./design_item/*.svelte', {
 		eager: true
 	});
 
 	const designItems: Component[] = Object.values(paths).map((path) => path.default);
-	const designItemContainerClass: string =
-		'flex h-fit w-full items-center justify-center gap-2.5 bg-white md:w-fit md:flex-col md:gap-4';
-
-	let screenWidth = $derived(innerWidth.current ?? 0);
-	let isLoading: boolean = $state(true);
-
-	$effect.pre(() => {
-		tick().then(() => {
-			isLoading = false;
-		});
-	});
 </script>
 
 <main>
@@ -35,67 +20,17 @@
 	>
 		Powered by Svelte and Tailwind CSS
 	</p>
-	<section class="flex w-full flex-col justify-center gap-4 py-8 md:flex-row md:py-12">
-		{#if isLoading}
-			<SvgSpinnersBarsRotateFade
-				className="size-10 text-zinc-500 absolute left-1/2 top-1/2 -translate-1/2"
-			/>
-		{:else if !isLoading}
-			{#if screenWidth >= 1280}
-				{@render pseudoMasonry(3)}
-			{:else if screenWidth >= 768}
-				{@render pseudoMasonry(2)}
-			{:else}
-				{@render pseudoMasonry(1)}
-			{/if}
-		{/if}
+	<section class="content mx-auto w-fit columns-1 gap-4 py-8 md:columns-2 md:py-12 lg:columns-3">
+		{#each designItems as Item}
+			<div
+				class="mb-2.5 flex w-[18.75rem] break-inside-avoid items-center justify-center rounded-xl border
+					border-zinc-200 bg-zinc-50 p-6.25 md:mb-4 md:w-[23.4375rem]"
+			>
+				<Item />
+			</div>
+		{/each}
 	</section>
 </main>
-
-{#snippet designItemComponent(Item: Component)}
-	<div
-		class="flex h-fit w-[300px] items-center justify-center rounded-xl border border-zinc-200 bg-zinc-50 p-6.25
-			md:w-[375px]"
-	>
-		<Item />
-	</div>
-{/snippet}
-
-{#snippet pseudoMasonry(columnNums: 1 | 2 | 3)}
-	{#if columnNums === 1}
-		{#each designItems as Item}
-			<div class={designItemContainerClass}>
-				{@render designItemComponent(Item)}
-			</div>
-		{/each}
-	{:else if columnNums === 2}
-		{#each { length: 2 }, i}
-			<div class={designItemContainerClass}>
-				{#each designItems as Item, index}
-					{#if index % 2 === 0 && i === 0}
-						{@render designItemComponent(Item)}
-					{:else if index % 2 !== 0 && i === 1}
-						{@render designItemComponent(Item)}
-					{/if}
-				{/each}
-			</div>
-		{/each}
-	{:else}
-		{#each { length: 3 }, i}
-			<div class={designItemContainerClass}>
-				{#each designItems as Item, index}
-					{#if index % 3 === 0 && i === 0}
-						{@render designItemComponent(Item)}
-					{:else if index % 3 === 1 && i === 1}
-						{@render designItemComponent(Item)}
-					{:else if index % 3 === 2 && i === 2}
-						{@render designItemComponent(Item)}
-					{/if}
-				{/each}
-			</div>
-		{/each}
-	{/if}
-{/snippet}
 
 <style>
 	.title,

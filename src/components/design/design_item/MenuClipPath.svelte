35f9d9835937c1756f0071ed9f62d5ec0f7d8d64
frsswq <farissaifuddin.work@gmail.com>
2025-04-10 13:06:11 +0700

    chore: optimize clip-path

--- src/components/design/design_item/MenuClipPath.svelte
+++ src/components/design/design_item/MenuClipPath.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { onMount, type Component } from 'svelte';
+	import { type Component } from 'svelte';
 
 	import Pinterest from '../../icons/MynauiBrandPinterest.svelte';
 	import Spotify from '../../icons/MynauiBrandSpotify.svelte';
 	import Dribble from '../../icons/MynauiBrandDribbble.svelte';
 
-	let width: number = $state(0);
 	let activeButton: number = $state(0);
 
 	const icons: { Icon: Component; text: string }[] = [
@@ -13,37 +12,6 @@
 		{ Icon: Pinterest, text: 'Pinterest' },
 		{ Icon: Spotify, text: 'Spotify' }
 	];
-
-	const clipPath: { desktop: string[]; mobile: string[] } = {
-		desktop: [
-			'inset(0 68.5% 0 1.75% round 0.4rem)',
-			'inset(0 32.5% 0 32.5% round 0.4rem)',
-			'inset(0 1.75% 0 68.5% round 0.4rem)'
-		],
-		mobile: [
-			'inset(0 69% 0 1% round 0.4rem)',
-			'inset(0 33% 0 32% round 0.4rem)',
-			'inset(0 1% 0 68% round 0.4rem)'
-		]
-	};
-
-	let currentClipPath: string = $derived(
-		width >= 768 ? clipPath.desktop[activeButton] : clipPath.mobile[activeButton]
-	);
-
-	onMount(() => {
-		width = window.innerWidth;
-
-		const handleResize = () => {
-			width = window.innerWidth;
-		};
-
-		window.addEventListener('resize', handleResize);
-
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	});
 </script>
 
 <div
@@ -54,7 +22,7 @@
 		<div
 			class="clip-button absolute z-[1] flex items-center justify-center gap-x-4 bg-orange-400 py-2 pr-3 pl-2
 				md:gap-x-6 md:pr-4 md:pl-3"
-			style="clip-path: {currentClipPath};"
+			data-active-button={activeButton}
 		>
 			{#each icons as { Icon, text }}
 				<button class="flex cursor-pointer items-center justify-center gap-x-0.5 text-white">
@@ -83,5 +51,27 @@
 	.clip-button {
 		transition: clip-path 0.15s cubic-bezier(0.22, 1, 0.36, 1);
 		will-change: clip-path;
+
+		&[data-active-button='0'] {
+			clip-path: inset(0 68.5% 0 1.75% round 0.4rem);
+		}
+		&[data-active-button='1'] {
+			clip-path: inset(0 32.5% 0 32.5% round 0.4rem);
+		}
+		&[data-active-button='2'] {
+			clip-path: inset(0 1.75% 0 68.5% round 0.4rem);
+		}
+
+		@media (width < 48rem) {
+			&[data-active-button='0'] {
+				clip-path: inset(0 68.5% 0 1.75% round 0.4rem);
+			}
+			&[data-active-button='1'] {
+				clip-path: inset(0 33% 0 32% round 0.4rem);
+			}
+			&[data-active-button='2'] {
+				clip-path: inset(0 1% 0 68% round 0.4rem);
+			}
+		}
 	}
 </style>

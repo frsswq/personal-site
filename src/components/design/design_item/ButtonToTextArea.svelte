f7bf9653f89da549ead25e4f096ebd72a489a384
frsswq <farissaifuddin.work@gmail.com>
2025-04-21 19:30:58 +0700

    add styling to morphing button

--- src/components/design/design_item/ButtonToTextArea.svelte
+++ src/components/design/design_item/ButtonToTextArea.svelte
@@ -4,18 +4,28 @@
 
 	let isOpen: boolean = $state(false);
 	let textArea: string = $state('');
+
+	$effect(() => {
+		if (!isOpen) {
+			textArea = '';
+		}
+	});
+
+	const quintOut: string = 'ease-[cubic-bezier(0.22, 1, 0.36, 1)]';
 </script>
 
 <div
-	class="flex min-h-[200px] min-w-[250px] items-center justify-center bg-transparent md:min-h-[275px]
-		md:min-w-[325px]"
+	class="relative flex min-h-[200px] min-w-[250px] items-center justify-center bg-transparent
+		md:min-h-[275px] md:min-w-[325px]"
 >
 	<button
 		class={cn(
-			`ease-[cubic-bezier(0.22, 1, 0.36, 1)] relative flex h-10 w-[125px] origin-center cursor-pointer
-			rounded-[5rem] border border-zinc-200 bg-white transition-all duration-300`,
+			`${quintOut} relative flex h-10 w-[125px] origin-center cursor-pointer rounded-[2rem] border
+			border-zinc-200 bg-white text-sm font-medium tracking-tight transition-all duration-[400ms]
+			md:text-base`,
 			{
-				'h-[200px] w-[250px] rounded-md md:h-[275px] md:w-[325px]': isOpen
+				[`h-[150px] w-[250px] cursor-default rounded-xl inset-ring-4 inset-ring-zinc-100 md:h-[225px]
+				md:w-[325px]`]: isOpen
 			}
 		)}
 		onmousedown={() => (isOpen = true)}
@@ -23,27 +33,30 @@
 	>
 		<span
 			class={cn(
-				` ease-[cubic-bezier(0.22, 1, 0.36, 1)] absolute top-1/2 left-1/2 -translate-1/2 text-base
-				font-medium tracking-tight transition-all duration-300`,
+				`${quintOut} absolute transition-all duration-[400ms]`,
 				{
 					'top-3 left-4 origin-top-left translate-0 text-zinc-400': isOpen
 				},
 				{
-					'origin-center': !isOpen
+					'top-1/2 left-1/2 origin-center -translate-1/2': !isOpen
 				}
 			)}
 		>
 			{textArea ? '' : 'Feedback'}
 		</span>
-		<textarea
-			class="top-3 left-4 h-full w-full resize-none border-none outline-none"
-			required
-			minlength="1"
-			spellcheck="false"
-			bind:value={textArea}
-		></textarea>
+		{#if isOpen}
+			<textarea
+				class="absolute top-3 left-4 h-3/4 w-[calc(100%-2rem)] resize-none overflow-hidden border-none
+					leading-tight outline-none"
+				required
+				minlength="1"
+				maxlength="300"
+				spellcheck="false"
+				bind:value={textArea}
+			></textarea>
+		{/if}
 	</button>
-	<p class="absolute bottom-3 text-[10px] text-zinc-700">
+	<p class="absolute -bottom-3 text-xs text-zinc-500 md:text-zinc-700">
 		remake of <a class="hover:text-sky-500" href="https://x.com/emilkowalski_" target="_blank"
 			>@emilkowalski_</a
 		> design
